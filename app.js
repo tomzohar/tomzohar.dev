bedeba0d8bb5cf9231bc144f1e3a8479fcfbc9f7
tomzohar <ttomz4291@gmail.com>
2024-05-31 20:03:01 +0300

    local pc test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
-http.createServer(function(req,res){
-	res.write("On the way to be a Fullstack engineer!\n I'm now connected to github remote :\)");
+http.createServer(function (req, res) {
+	res.write("On the way to be a Fullstack engineer!\n I'm now connected to github remote :\)\nThis code was pushed from local dev pc");
 	res.end();
 }).listen(3000);
 
