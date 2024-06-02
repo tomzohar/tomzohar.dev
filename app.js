0050331a156cb519962d53d4a24efd39718e46b0
tomzohar <ttomz4291@gmail.com>
2024-06-02 15:42:12 +0300

    Created a socket server and client

--- app.js
+++ app.js
@@ -3,5 +3,4 @@ http.createServer(function (req, res) {
 	res.write("On the way to be a Fullstack engineer!\n I'm now connected to github remote :\)\nThis code was pushed from local dev pc");
 	res.end();
 }).listen(3000);
-
 console.log('Server started on port 3000');
