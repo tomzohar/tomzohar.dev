aa327533f2c3b1616af3abe5f61bd10b5c277846
tomzohar <ttomz4291@gmail.com>
2024-06-03 09:07:18 +0300

    Add database loggin

--- index-ws.js
+++ index-ws.js
@@ -13,6 +13,18 @@ server.listen(3000, function () {
 	console.log('Server started on port 3000');
 });
 
+
+process.on('SIGINT', () => {
+	console.log('sigint');
+	wss.clients.forEach(function each(client) {
+		client.close();
+	});
+	server.close(() => {
+		shutdownDB();
+	});
+});
+
+
 /**
  * Begin Websocket
  */
@@ -30,6 +42,11 @@ wss.on('connection', function connection(ws) {
 		ws.send('Welcome to my server');
 	}
 
+	db.run(`
+		INSERT INTO visitors (count, time)
+		VALUES (${numClients}, datetime('now'))
+	`);
+
 	ws.on('close', function close() {
 		console.log('A client has disconnected');
 	});
@@ -40,4 +57,31 @@ wss.broadcast = function broadcast(data) {
 	wss.clients.forEach(function each(client) {
 		client.send(data);
 	});
+}
+
+/** End WebSockets */
+
+/** Begin database */
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+	db.run(`
+		CREATE TABLE visitors (
+			count INTEGER,
+			time TEXT
+		)
+	`);
+});
+
+function getCount() {
+	db.each(`SELECT * FROM visitors`, (err, row) => {
+		console.log(row);
+	});
+}
+
+function shutdownDB() {
+	getCount();
+	console.log(`Shutting down db`);
+	db.close();
 }
